9ee816782e561a2f1cbfc108d89560721a6ce28f
abrojodigital <abrojodigital@gmail.com>
2020-07-08 10:25:40 -0300

    Ajustes en resizer

--- Resize Img/resizer.js
+++ Resize Img/resizer.js
@@ -1,38 +1,39 @@
-const fs = require("fs").promises
-const path = require("path")
-const sharp = require("sharp")
+const fs = require("fs").promises;
+const path = require("path");
+const sharp = require('sharp')
 
-const resizeImg = await (ruta) => {
+const resizeImg = async (ruta) => {
   // acá tengo que ver la libreria
 }
+
 const recorrerDirectorio = async (rutaDir) => {
-  const rutas = await fs.readdir(rutaDir)
+  const rutas = await fs.readdir(rutaDir);
   for (let index = 0; index < rutas.length; index++) {
-    const rutaAbsoluta = path.join(rutaDir, rutas[index])
+    const rutaAbsoluta = path.join(rutaDir, rutas[index]);
     try {
-      await procesarRuta(rutaAbsoluta)
+      await procesarRuta(rutaAbsoluta);
     } catch (_) {}
   }
 };
 
 const procesarRuta = async (ruta) => {
-  const stats = await fs.stat(ruta)
+  const stats = await fs.stat(ruta);
   if (stats.isDirectory()) {
-    return recorrerDirectorio(ruta)
+    return recorrerDirectorio(ruta);
   } else if (stats.isFile()) {
-    return resizeImg(ruta)
+    return await resizeImg(ruta);
   }
-}
+};
 
 (async (_) => {
   try {
-    const ordenesPath = path.normalize(process.argv[2])
-    const ordenesPathAbsoluto = path.isAbsolute(ordenesPath)
-      ? ordenesPath
-      : path.join(process.cwd(), ordenesPath)
-    //await procesarPath(ordenesPathAbsoluto)
+    const imgsPath = path.normalize(process.argv[2])
+    const imgsPathAbsoluto = path.isAbsolute(imgsPath)
+      ? imgsPath
+      : path.join(process.cwd(), imgsPath)
+    await procesarPath(imgsPathAbsoluto)
     process.stdout.cursorTo(0)
-    console.log(ordenesPathAbsoluto)
+    // console.log(imgsPathAbsoluto)
   } catch (error) {
     process.stdout.cursorTo(0)
     console.log(`ERROR: ${error.message}`)

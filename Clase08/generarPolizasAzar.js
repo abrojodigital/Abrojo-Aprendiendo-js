ff676ec5c3d8f8ebf6303cfa77ce8d41471fc26b
abrojodigital <abrojodigital@gmail.com>
2020-07-08 10:15:14 -0300

    Ajustes en Clase-008

--- Clase08/generarPolizasAzar.js
+++ Clase08/generarPolizasAzar.js
@@ -1,83 +1,103 @@
-const fs = require('fs').promises
-const path = require('path')
-const crypto = require('crypto')
-const { parse } = require('path')
+const fs = require("fs").promises;
+const path = require("path");
+const crypto = require("crypto");
+const { parse } = require("path")
 
-let archivosCreados = 0
-const archivosMaximosACrear = 200
-const maximaProfundidadDirectorios = 5
-const directorioSalida = path.join(process.cwd(), 'input')
+let archivosCreados = 0;
+const archivosMaximosACrear = 200;
+const maximaProfundidadDirectorios = 5;
+const directorioSalida = path.join(process.cwd(), "input");
 
-let marcas =[]
-let modelos = []
+let marcas = [];
+let modelos = [];
 
 const cargarMarcasModelos = async () => {
-    try {
-        const archivo= path.resolve(__dirname, "MarcasModelos.json")
-        const archivoMarcasModelos = await fs.readFile(archivo)
-        const MarcasModelos = JSON.parse(archivoMarcasModelos)
-        marcas = MarcasModelos.marcas
-        modelos = MarcasModelos.modelos
+  try {
+    const archivo = path.resolve(__dirname, "MarcasModelos.json");
+    const archivoMarcasModelos = await fs.readFile(archivo);
+    const MarcasModelos = JSON.parse(archivoMarcasModelos);
+    marcas = MarcasModelos.marcas;
+    modelos = MarcasModelos.modelos;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-    }
-}
+const tipoSeguros = ["TR", "CT", "TC"];
 
-const tipoSeguros = ['TR', 'CT', 'TC']
+const incrementarArchivosCreados = (_) => {
+  archivosCreados++;
+  process.stdout.cursorTo(0);
+  process.stdout.write(`${archivosCreados} / ${archivosMaximosACrear}`);
+};
 
-const incrementarArchivosCreados = _ => {
-  archivosCreados++
-  process.stdout.cursorTo(0)
-  process.stdout.write(`${archivosCreados} / ${archivosMaximosACrear}`)
-}
-
-const archivoDataRandom = _ => {
-  let aleatorioMarca = Math.floor(Math.random()*(marcas.length))
-  let marca = marcas[aleatorioMarca]
-  let modelo = modelos[aleatorioMarca][Math.floor(Math.random()*(modelos[aleatorioMarca].length))]
-  let aleatorioSeguro = tipoSeguros[Math.floor(Math.random()*(tipoSeguros.length))]
+const archivoDataRandom = (_) => {
+  let aleatorioMarca = Math.floor(Math.random() * marcas.length);
+  let marca = marcas[aleatorioMarca];
+  let modelo =
+    modelos[aleatorioMarca][
+      Math.floor(Math.random() * modelos[aleatorioMarca].length)
+    ];
+  let aleatorioSeguro =
+    tipoSeguros[Math.floor(Math.random() * tipoSeguros.length)];
 
   return {
     marca: marca,
     modelo: modelo,
-    valor:parseFloat((Math.random() * 2000000).toFixed(2)),
-    año: parseInt((Math.random() * 10) + 2010),
-    tipoSeguro: aleatorioSeguro
-  }
-}
+    valor: parseFloat((Math.random() * 2000000).toFixed(2)),
+    año: parseInt(Math.random() * 10 + 2010),
+    tipoSeguro: aleatorioSeguro,
+  };
+};
 
 const archivoDirectorioRandomico = async (directorioActual) => {
   if (archivosCreados < archivosMaximosACrear) {
-    const random = Math.random()
-    if (random < 0.80) {
-      incrementarArchivosCreados()
-      const archivoRandom = 'auto'+crypto.randomBytes(parseInt(Math.random() * 20) + 1).toString('hex')
-      const archivoPathAbsoluto = path.join(directorioActual, `${archivoRandom}.json`)
-      const archivoData = archivoDataRandom()
-      await fs.mkdir(directorioActual, { recursive: true })
-      await fs.writeFile(archivoPathAbsoluto, Buffer.from(JSON.stringify(archivoData, null, 4)))
-      await archivoDirectorioRandomico(directorioActual)
+    const random = Math.random();
+    if (random < 0.8) {
+      incrementarArchivosCreados();
+      const archivoRandom =
+        "auto" +
+        crypto.randomBytes(parseInt(Math.random() * 20) + 1).toString("hex");
+      const archivoPathAbsoluto = path.join(
+        directorioActual,
+        `${archivoRandom}.json`
+      );
+      const archivoData = archivoDataRandom();
+      await fs.mkdir(directorioActual, { recursive: true });
+      await fs.writeFile(
+        archivoPathAbsoluto,
+        Buffer.from(JSON.stringify(archivoData, null, 4))
+      );
+      await archivoDirectorioRandomico(directorioActual);
     } else {
-      const profundidad = path.relative(directorioSalida, directorioActual).split(path.sep).length
-      if (random < 0.90 && profundidad < maximaProfundidadDirectorios) {
-        const directorioRandom = crypto.randomBytes(parseInt(Math.random() * 11)+1).toString('hex')
-        const directorioPathAbsoluto = path.join(directorioActual, directorioRandom)
-        await archivoDirectorioRandomico(directorioPathAbsoluto)
+      const profundidad = path
+        .relative(directorioSalida, directorioActual)
+        .split(path.sep).length;
+      if (random < 0.9 && profundidad < maximaProfundidadDirectorios) {
+        const directorioRandom = crypto
+          .randomBytes(parseInt(Math.random() * 11) + 1)
+          .toString("hex");
+        const directorioPathAbsoluto = path.join(
+          directorioActual,
+          directorioRandom
+        );
+        await archivoDirectorioRandomico(directorioPathAbsoluto);
       } else if (profundidad > 1) {
-        await archivoDirectorioRandomico(path.join(directorioActual, '..'))
+        await archivoDirectorioRandomico(path.join(directorioActual, ".."));
       } else {
-        await archivoDirectorioRandomico(directorioActual)
+        await archivoDirectorioRandomico(directorioActual);
       }
     }
   } else {
-    process.stdout.cursorTo(0)
-    console.log(`Creados ${archivosMaximosACrear} archivos de pólizas de autos`)
+    process.stdout.cursorTo(0);
+    console.log(
+      `Creados ${archivosMaximosACrear} archivos de pólizas de autos`
+    );
   }
-}
+};
 
-;(async _ => {
-  await cargarMarcasModelos()
-  await fs.rmdir(directorioSalida, { recursive: true })
-  await archivoDirectorioRandomico(directorioSalida)
-})()
+(async (_) => {
+  await cargarMarcasModelos();
+  await fs.rmdir(directorioSalida, { recursive: true });
+  await archivoDirectorioRandomico(directorioSalida);
+})();
--- Clase08/verAsyncAwait.js
+++ Clase08/verAsyncAwait.js
@@ -1,8 +1,8 @@
-const fs = require("fs").promises
-const path = require("path")
+const fs = require('fs').promises
+const path = require('path')
 
-const directorioIn = path.join(__dirname, "input")
-const archivoOut = path.join(__dirname, "totales.json")
+const directorioIn = path.join(__dirname, 'input')
+const archivoOut = path.join(__dirname, 'totales.json')
 
 const totales = {
   totalValorAutos: 0,
@@ -21,21 +21,21 @@ const recorrerDirectorio = async (rutaDir) => {
 }
 
 const validarEsAuto = async (ruta) => {
-  if (ruta.endsWith(".json")) {
+  if (ruta.endsWith('.json')) {
     const data = await fs.readFile(ruta)
     const auto = JSON.parse(data)
-    if (!auto.hasOwnProperty("tipoSeguro")) {
-      throw new Error("Al auto le falta Tipo de Seguro.")
-    } else if (!auto.hasOwnProperty("valor")) {
-      throw new Error("Al auto le falta Valor.")
-    } else if (!auto.hasOwnProperty("año")) {
-      throw new Error("Al auto le falta Año.")
+    if (!auto.hasOwnProperty('tipoSeguro')) {
+      throw new Error('Al auto le falta Tipo de Seguro.')
+    } else if (!auto.hasOwnProperty('valor')) {
+      throw new Error('Al auto le falta Valor.')
+    } else if (!auto.hasOwnProperty('año')) {
+      throw new Error('Al auto le falta Año.')
     } else {
-        procesarAuto(auto)
+      procesarAuto(auto)
     }
   }
-  throw new Error("No es un archivo de auto válido")
-}
+  throw new Error('No es un archivo de auto válido')
+};
 
 const procesarRuta = async (ruta) => {
   const stats = await fs.stat(ruta)
@@ -46,33 +46,33 @@ const procesarRuta = async (ruta) => {
   }
 }
 
-const procesarAuto = async (auto) => {
-  totales.totalSeguros += await calcularSeguro(auto)
+const procesarAuto =ers (auto) => {
+  totales.totalSeguros += calcularSeguro(auto)
   totales.cantAutosProcesados++
   totales.totalValorAutos += auto.valor
 }
 
-const calcularSeguro = async (auto) => {
+const calcularSeguro = (auto) => {
   const añoActual = new Date().getFullYear()
   let seguro = 0
-  if (auto.tipoSeguro === "TR") {
+  if (auto.tipoSeguro === 'TR') {
     // Verifico si eligió Todo Riesgo
     if (añoActual - auto.año <= 10) {
       // verifico si el auto es mas viejo que 10 años
       seguro = auto.valor * 0.01 // calculo el 1% de valor del auto
     } else {
-      seguro = 10000 //valor alto porque el auto es muy viejo
+      seguro = 10000 // valor alto porque el auto es muy viejo
     }
   } else {
-    if (auto.tipoSeguro === "TC") {
+    if (auto.tipoSeguro === 'TC') {
       seguro = auto.valor * 0.0025 // Terceros Completo
     } else {
       // Otro seguro. Ejemplo Contra Terceros
       seguro = 800
     }
   }
-  return seguro;
-};
+  return seguro
+}
 
 const guardarObjeto = async (ruta, totales) => {
   const datos = JSON.stringify(totales)
